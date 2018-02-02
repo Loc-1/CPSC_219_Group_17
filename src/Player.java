17dd2fef83c0ba72b10e509bb386f2d7564d3cda
Josh Harkema <josh@joshharkema.com>
2018-02-02 09:13:52 -0700

    Skeleton of player class finished.

--- src/Player.java
+++ src/Player.java
@@ -1,23 +1,47 @@
+/**
+ * The player class handles the player's location on the board, as well as tracking the player's id, handle, and
+ * current score. Player movement is handled via move methods defined in this class.
+ */
 public class Player {
-    private int user_id;
-    private int y_location;
-    private int x_location;
+    private int userID;
+    private String userHandle;
+    private int yLocation;
+    private int xLocation;
     private int score = 0;
-    private String handle;
+    private int sprite; // :TODO: Change to a sprite class once one is implemented.
 
-    public void setHandle() {
+    /***
+     * Constructor to add player sprite to the board.
+     * @param setXLocation player's starting x coordinate.
+     * @param setYLocation player's starting y coordinate.
+     * @param setSprite player's character sprite.
+     * @param setUserHandle player's chosen handle.
+     */
+    public Player(int setXLocation, int setYLocation, int setSprite, String setUserHandle) {
+        this.xLocation = setXLocation;
+        this.yLocation = setYLocation;
+        this.sprite = setSprite;
+        this.userHandle = setUserHandle;
+    }
+
+    public void setUserHandle(String userHandle) {
+        this.userHandle = userHandle;
     }
 
     public void moveUp() {
+        this.xLocation += 1;
     }
 
     public void moveDown() {
+        this.xLocation -= 1;
     }
 
     public void moveRight() {
+        this.yLocation += 1;
     }
 
     public void moveLeft() {
+        this.yLocation -= 1;
     }
 
 }
