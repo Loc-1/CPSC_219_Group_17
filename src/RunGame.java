45080d29e7eb6c2acbc468af1631fc8f73310e6d
Lachlan <Lachlan@DESKTOP-PQ17Q09.hitronhub.home>
2018-02-06 12:37:33 -0700

    Started working on the run wrapper

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,8 +1,15 @@
 public class RunGame {
     
-    /*
-     * Not sure how to begin this class. 
-     * 
-     */
+    public static void main(String[] args) {
+        Board gameBoard = new Board(5,10,1);
+        Player p1 = new Player(10, 3, 1, 001,"");
+        
+        boolean gameRunning = true;
+        while (gameRunning == true) {
+            
+        }
+        
+        
+    }
     
 }
