eade7e7246810e188488f53d6a11d655abf11894
Lachlan <Lachlan@DESKTOP-PQ17Q09.hitronhub.home>
2018-02-07 13:00:37 -0700

    small changes

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,11 +1,14 @@
 public class RunGame {
     
     public static void main(String[] args) {
-        Player p1 = new Player(10, 3, 1, 001,"");
+        System.out.println("Please enter your name.");
+        String userHandle = "";
+        Player p1 = new Player(10, 3, 1, 001,userHandle);
         Board gameBoard = new Board(5, 10, 1, p1);
 
         boolean gameRunning = true;
         while (gameRunning == true) {
+            gameBoard.printBoard();
             
         }
         
