2a596be146e38991cfb7835735be125fa66d25c0
Ashton-rice <35851499+Ashton-rice@users.noreply.github.com>
2018-02-09 15:53:05 -0700

    changed to skeleton framework

--- src/PowerUpTest.java
+++ src/PowerUpTest.java
@@ -1,5 +1,25 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+//testing the PowerUp class
 
 public class PowerUpTest {
 
+
+	// test for setType() method
+	@Test
+	public void test_set_type(){
+
+	}
+	
+	
+	//test for setLocation() method
+	@Test
+	public void test_set_location(){
+
+	
+	}
+
+  
 }
