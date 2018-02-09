38a9d5fb000b6853469ca615c2734a389b446aad
Ashton-rice <35851499+Ashton-rice@users.noreply.github.com>
2018-02-09 13:45:21 -0700

    Add files via upload

---
+++ src/EnemyTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+//testing the Enemy class
+
+public class EnemyTest {
+
+
+	// test for setDirection() method
+	@Test
+	public void test_set_direction(){
+
+	}
+	
+	
+	//test for setStartLocation() method
+	@Test
+	public void test_start_location(){
+	
+	}
+	
+	
+		// test for setEndLocation() method
+	@Test
+	public void test_end_location(){
+
+	}
+	
+	
+	//test for setSpeed() method
+	@Test
+	public void test_set_speed(){
+	
+	}
+	
+	
+
+
+
+
+}
---
+++ src/HighScoresTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+//testing the HighScores class
+
+public class HighScoresTest {
+
+
+	// test for set() method
+	@Test
+	public void test_set_highScore(){
+
+	}
+	
+	
+	//test for save() method
+	@Test
+	public void test_score_save(){
+	
+	}
+	
+	
+}
