d8a11af60e737d7ffafdfbd0f384ab2616b25377
Lachlan <Lachlan@DESKTOP-PQ17Q09.hitronhub.home>
2018-02-12 11:19:39 -0700

    Added more changes to GUI design

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,7 +1,23 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
+/**
+ * Class Owner: Lachlan
+ * 
+ * This class is a frame to display the menus and game windows. Includes a GUI
+ * to display game to users.
+ *
+ */
 public class GameWindow {
 
     private JFrame frame;
@@ -34,8 +50,26 @@ public class GameWindow {
      */
     private void initialize() {
         frame = new JFrame();
-        frame.setBounds(100, 100, 450, 300);
+        frame.setBounds(100, 100, 522, 557);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
 
+        JPanel panel = new JPanel();
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+        JButton startButton = new JButton("Start Game");
+        JButton tutorialButton = new JButton("Tutorial");
+        
+        GroupLayout gl_panel = new GroupLayout(panel);
+        gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+                .addGroup(gl_panel.createSequentialGroup()
+                        .addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+                                .addGroup(gl_panel.createSequentialGroup().addGap(197).addComponent(startButton))
+                                .addGroup(gl_panel.createSequentialGroup().addGap(26).addComponent(tutorialButton)))
+                        .addContainerGap(210, Short.MAX_VALUE)));
+        gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+                .addGroup(gl_panel.createSequentialGroup().addGap(225).addComponent(startButton)
+                        .addPreferredGap(ComponentPlacement.RELATED, 196, Short.MAX_VALUE).addComponent(tutorialButton)
+                        .addGap(39)));
+        panel.setLayout(gl_panel);
+    }
 }
