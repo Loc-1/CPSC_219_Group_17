3f9293bd0ace8d3bc54a112a3b39fb7b99fae5b4
Josh Harkema <josh@joshharkema.com>
2018-02-22 19:50:12 -0700

    Added notes.

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,10 +1,22 @@
 import java.util.concurrent.TimeUnit;
 
+/**
+ * This is the primary game loop, it constructs a new game (Board, Player, and BoardWindow.)
+ */
 public class RunGame {
     private Board board;
     private Player playerOne;
     private Boolean isRunning = true;
 
+    /**
+     * Constructs a new board.
+     *
+     * @param rows       the desired number of rows.
+     * @param cols       the desired number of columns.
+     * @param difficulty the desired difficulty.
+     * @param handle     the player's handle. :TODO: Add a second player.
+     * @throws InterruptedException if the while loop is interrupted.
+     */
     public RunGame(int rows, int cols, int difficulty, String handle) throws InterruptedException {
         this.playerOne = new Player(rows - 1, cols / 2, 1, 1, handle);
         this.board = new Board(rows, cols, difficulty, this.playerOne);
@@ -17,6 +29,8 @@ public class RunGame {
         }
 
         this.isRunning = false;
+        boardWindow.trash();
+
     }
 
     public static void main(String[] args) throws InterruptedException {
